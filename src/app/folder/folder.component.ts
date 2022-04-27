4e3eb3afe652b033b9d973719ae8441a60b7e622
Krupa Patel <53213815+krupa253@users.noreply.github.com>
2022-04-27 18:02:06 +0530

    Static Folder done

--- src/app/folder/folder.component.ts
+++ src/app/folder/folder.component.ts
@@ -1,17 +1,4 @@
-import { Component, Injectable, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { BehaviorSubject, Observable } from 'rxjs';
-
-// export class FileNode {
-//   children!: FileNode[];
-//   filename!: string;
-//   type: any;
-// }
-
-export class FileFlatNode {
-  constructor(
-    public expandable: boolean, public filename: string, public level: number, public type: any) {}
-}
+import { Component, OnInit } from '@angular/core';
 
 export interface FileNode {
   name: string;
@@ -26,7 +13,7 @@ export interface FileNode {
 
 export class FolderComponent implements OnInit {
 
-  TREE_DATA = [
+  public TREE_DATA = [
     {
       name: "Fruit",
       children: [{ name: "Apple" }, { name: "Banana" }, { name: "Fruit loops" }]
@@ -41,39 +28,26 @@ export class FolderComponent implements OnInit {
         {
           name: "Orange",
           children: [{ name: "Pumpkins" }, { name: "Carrots" }]
+        },
+        {
+          name: "Vegetables",
+          children: [
+            {
+              name: "Green",
+              children: [{ name: "Broccoli" }, { name: "Brussel sprouts" }]
+            },
+            {
+              name: "Orange",
+              children: [{ name: "Pumpkins" }, { name: "Carrots" }]
+            }
+          ]
         }
       ]
     }
   ];
 
-  // public inputfolder = [TREE_DATA];
-  // folderDetails!: any;
-  // showform = false;
-
-  // constructor(private formBuilder: FormBuilder) { 
-  //   this.folderDetails = this.formBuilder.group({
-  //     firstfolder: ['', [Validators.required]],
-  //     choice: ['', [Validators.required]]
-  //   });
-  // }
-
   ngOnInit(): void {
     console.log(this.TREE_DATA);
   }
-
-  // add_more_field() {
-  //   this.showform = true;
-  //   this.folderDetails.controls['choice'].setValue('folder');
-  // }
-
-  // removeAddress(i: number) {
-  //   this.inputfolder.splice(i, 1);
-  // }
-
-  // adddata() {
-  //   const treeData = this.folderDetails.value;
-  //   this.inputfolder.push(treeData);
-  //   console.log(treeData);
-  // }
 
 }
