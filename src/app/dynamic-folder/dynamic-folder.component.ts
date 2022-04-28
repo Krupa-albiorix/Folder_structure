17d770876cfd1a49dc10c078d20f4a03027e9581
Krupa Patel <53213815+krupa253@users.noreply.github.com>
2022-04-28 16:16:40 +0530

    root added

--- src/app/dynamic-folder/dynamic-folder.component.ts
+++ src/app/dynamic-folder/dynamic-folder.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { resetFakeAsyncZone } from '@angular/core/testing';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-folder',
@@ -7,17 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DynamicFolderComponent implements OnInit {
 
+  show_form = false;
   public inputfolder: any[] = [];
+  folderdetails!: any;
 
-  constructor() { }
+  constructor( private formBuilder : FormBuilder ) { 
+    this.folderdetails = this.formBuilder.group({
+      choice: ['', [Validators.required]],
+      name: ['', [Validators.required]]
+    })
+  }
 
   ngOnInit(): void {
-    console.log(this.inputfolder);
+    // console.log(this.inputfolder);
   }
 
   add_more_field() {
-    this.inputfolder.push({
-    });
+    this.show_form = true;
+    this.folderdetails.controls['choice'].setValue('folder');
+  }
+
+  add_data() {
+    this.inputfolder.push(this.folderdetails.value);
+    console.log(this.folderdetails);
+    this.show_form = false;
+    this.folderdetails.reset();
   }
 
   removeAddress(i: number) {
